b4b8c4cb25430d38d7f9edf2b4539d2265162cf2
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-10 15:08:15 +0200

    Exercise solved

---
+++ db/migrate/20170510120754_change_content_from_reviews.rb
@@ -0,0 +1,5 @@
+class ChangeContentFromReviews < ActiveRecord::Migration[5.0]
+  def change
+    change_column :reviews, :content, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0655234937",
+    category:     "chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "0658254937",
+    category:     "chinese"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "0655254927",
+    category:     "italian"
+  },
+  {
+    name:         "Chez Momo",
+    address:      "112 rue du Bourg 75008 Paris",
+    phone_number: "0655874937",
+    category:     "japanese"
+  },
+  {
+    name:         "Kebab Nico",
+    address:      "8 rue Texier 75005 Paris",
+    phone_number: "0656374937",
+    category:     "french"
+  },
+  {
+    name:         "Le bistrot regent",
+    address:      "place Gambetta 33000 Bordeaux",
+    phone_number: "0655566427",
+    category:     "belgian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts "Finished!"
